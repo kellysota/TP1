44ab436e42c1f6e7f563588a3da5dc4608bf23ab
Kelly Sota <nsota25@gmail.com>
2024-10-15 21:13:30 +0200

    Toutes les fonctions works wellgit add task_manager.py

---
+++ task_manager.py
@@ -0,0 +1,44 @@
+tasks = []
+
+def add_task():
+    task = input("Enter a new task: ")
+    tasks.append(task)
+    print("Added task!")
+
+def show_tasks():
+    if not tasks:
+        print("Task list is empty.")
+    else:
+        for i, task in enumerate(tasks):
+            print(f"{i+1}. {task}")
+
+def mark_task_as_done():
+    """Mark a specific task as done."""
+    if not tasks:
+        print("No tasks to mark as done.")
+        return
+
+
+while True:
+    print("\nPick a task: ")
+    print("1. Display the tasks")
+    print("2.Add a task")
+    print("3. Mark a task as done")
+    print("4. DONE!")
+        
+    choice = input(">")
+
+    if choice == '1':
+        show_tasks()
+
+    elif choice == '2':
+        add_task()
+
+    elif choice == '3':
+        mark_task_as_done()
+
+    elif choice == '4':
+        break
+
+    else:
+        print("Invalide choice.")
